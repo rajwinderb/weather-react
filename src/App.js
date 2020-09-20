1b5667710d524e18953a32b85a4ba85527761a0d
rajwinderb <68147202+rajwinderb@users.noreply.github.com>
2020-09-20 19:10:32 +0100

    Warning check

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
             <Header />
             <Weather />
           </div>
-          <p className="code"> <a href="https://github.com/rajwinderb/weather-react" target="_blank">Open-source Code</a> by Rajwinder Bhatoe </p>
+          <p className="code"> <a href="https://github.com/rajwinderb/weather-react">Open-source Code</a> by Rajwinder Bhatoe </p>
         </div>
     </div>
   );
